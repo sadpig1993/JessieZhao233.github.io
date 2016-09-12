1bcf3ed1e28908279330816d09a09d4330557230
JessieZhao233 <2870085110@qq.com>
2016-09-12 14:11:43 +0800

    dd

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
-(function(){
-    var hash = location.hash.replace('#','');
-    if(hash == ''){
-        location.hash='#position';
-    }
-    $(window).on('hashchange',function(){
-        var hash = location.hash.replace('#','');
-        getArticle(hash);
-    });
-    function getArticle(hash){
-        $.ajax({
-            url: "./data/"+hash+".json",
-            type: "GET",
-            dataType:'json',
-            success:function(data){
-               	$("#welcomeMain").hide();
-                $('#view').html(data.content);
-            },
-            error:function(er){
-                console.log(er);
-            }
-        });
-    }
+(function() {
+	var hash = location.hash.replace('#', '');
+	if(hash == '') {
+		location.hash = '#position';
+	}
+	$("#moreToMainPage").on("click", function() {
+		location.hash = "#myCat";
+	});
+	$(window).on('hashchange', function() {
+		var hash = location.hash.replace('#', '');
+		getArticle(hash);
+	});
+
+	function getArticle(hash) {
+		$.ajax({
+			url: "./data/" + hash + ".json",
+			type: "GET",
+			dataType: 'json',
+			success: function(data) {
+				$("#welcomeMain").hide();
+				$('#view').html(data.content);
+			},
+			error: function(er) {
+				console.log(er);
+			}
+		});
+	}
 })();
